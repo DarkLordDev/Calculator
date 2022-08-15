5b4769b80c6188e981882e355f0e29c5acd99000
DarkLordDev <microansh2@gmail.com>
2022-08-15 13:15:37 +0530

    upd 0.0.2

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const screen = document.getElementById("screen");
 // console.log(buttons[1]);
 for (const button of buttons) {
 	button.addEventListener("mouseup", (e) => {
-		const screenText = screen.innerText;
-		console.log(e);
-		if (screenText !== "Enter Something here") {
+		// console.log(e);
+		if (e.target.id === "=") {
+			console.log(screen.innerText);
+			const result = eval(screen.innerText);
+			screen.innerText = result;
+		} else if (e.target.id === "sqrt") {
+			const result = Math.sqrt(screen.innerText);
+			screen.innerText = result;
+		} else if (e.target.id === "c") {
+			screen.innerText = "Enter Something here";
+		} else if (screen.innerText !== "Enter Something here") {
 			screen.innerText = screen.innerText + e.target.id;
 		} else {
 			screen.innerText = e.target.id;
